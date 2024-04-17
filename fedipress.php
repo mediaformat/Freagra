f99b4b5a8ee2f816d9153de7523b8a501251e490
Django Doucet <mediaformat.ux@gmail.com>
2024-04-16 23:33:18 -0600

    register settings page

--- fedipress.php
+++ fedipress.php
@@ -14,3 +14,31 @@
 
 namespace FediPress;
 
+
+/**
+ * register_plugin_settings_page
+ *
+ * @return void
+ */
+function register_plugin_settings_page() {
+	$page_hook_suffix = \add_options_page(
+		__( 'FediPress', 'fedipress' ),
+		__( 'FediPress', 'fedipress' ),
+		'manage_options',
+		'fedipress',
+		__NAMESPACE__ . '\render_settings_page',
+	);
+}
+\add_action( 'admin_menu', __NAMESPACE__ . '\register_plugin_settings_page' );
+
+/**
+ * Renders the settings page
+ */
+function render_settings_page() {
+	?>
+	<div id="fedipress">
+		<?php esc_html_e( 'Requires JavaScript', 'fedipress' ); ?>
+	</div>
+	<?php
+}
+
