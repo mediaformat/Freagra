21c6241ef55a4eb335641c978afcb493224d4e35
Django Doucet <mediaformat.ux@gmail.com>
2024-04-16 23:31:16 -0600

    Plugin boilerplate

---
+++ fedipress.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     FediPress
+ * Plugin URI:      https://codeberg.org/OpenForFuture/FediPress
+ * Description:     An ActivityPub chat PWA for WordPress.
+ * Author:          MediaFormat
+ * Author URI:      https://mediaformat.org
+ * Text Domain:     fedipress
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         FediPress
+ */
+
+namespace FediPress;
+
