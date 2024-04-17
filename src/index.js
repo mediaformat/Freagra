a2265df2e44d949e129f85d6d74fe17a61cb6032
Django Doucet <mediaformat.ux@gmail.com>
2024-04-16 23:36:31 -0600

    init app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * WordPress dependencies
+ */
+import { createRoot } from '@wordpress/element';
+
+// Render the app to the screen.
+const container = document.getElementById('fedipress');
+const root = createRoot(container);
